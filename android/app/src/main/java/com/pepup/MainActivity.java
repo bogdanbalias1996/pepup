7e8c00ca7848ee2fbd18bef8f549991db163d76a
bogdan <rom@bogdans-MacBook-Pro.local>
2019-11-02 11:03:57 +0200

    fix tabs with android

--- android/app/src/main/java/com/pepup/MainActivity.java
+++ android/app/src/main/java/com/pepup/MainActivity.java
@@ -2,6 +2,9 @@ package in.bitsport.pepup;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
@@ -18,5 +21,15 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "pepup";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,58 +1,67 @@
 import * as React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import { TabsProps, Tab } from './';
-import { ButtonGroup } from '../../components/ButtonGroup/ButtonGroup';
-import { ButtonGroupItem } from '../../components/ButtonGroup';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import {
+  colorVioletGrey,
+  colorBlack,
+  boldFont,
+  semiboldFont
+} from '../../variables';
 
 export class Tabs extends React.PureComponent<TabsProps> {
   state = {
-    activeTabIndex: this.props.activeTabIndex ? this.props.activeTabIndex : 0
+    index: this.props.activeTabIndex ? this.props.activeTabIndex : 0
   };
 
   render() {
-    const {
-      config,
-      style = {},
-      stylesTabsContainer = {},
-      stylesItem = {},
-      stylesItemText = {},
-      stylesSelectedItem = {},
-      stylesSelectedItemText = {}
-    } = this.props;
-    const { activeTabIndex } = this.state;
-    const TabComponent = config[activeTabIndex].component;
+    const { config, style = {} } = this.props;
 
-    const convertTabsToButtonGroupConfi = (
-      config: Array<Tab>
-    ): Array<ButtonGroupItem> => {
-      return config.map((item: Tab, index: number) => {
-        return {
-          value: index,
-          title: item.title || '',
-          component: item.titleComponent
-            ? (isActive: boolean) => item.titleComponent(isActive)
-            : null,
-          onPress: indexTab => {
-            console.log('state', indexTab);
-            if (item.onPress) item.onPress(indexTab);
-            this.setState({ activeTabIndex: indexTab });
-          }
-        };
-      });
-    };
+    const parseConfigToScenes = (config: Array<Tab>) =>
+      config.reduce((acc: { [key: string]: string }, cur, index) => {
+        acc[cur.title] = cur.component;
+        return acc;
+      }, {});
+
+    const parseConfigToRoutes = (config: Array<Tab>) =>
+      config.map((item: Tab, index: number) => ({
+        key: item.title,
+        title: item.title
+      }));
 
     return (
       <View style={style}>
-        <ButtonGroup
-          items={convertTabsToButtonGroupConfi(config)}
-          value={activeTabIndex}
-          style={stylesTabsContainer}
-          stylesItem={stylesItem}
-          stylesItemText={stylesItemText}
-          stylesSelectedItem={stylesSelectedItem}
-          stylesSelectedItemText={stylesSelectedItemText}
+        <TabView
+          lazy
+          navigationState={{
+            ...this.state,
+            routes: parseConfigToRoutes(config)
+          }}
+          renderScene={SceneMap(parseConfigToScenes(config))}
+          onIndexChange={index => this.setState({ index })}
+          initialLayout={{ width: Dimensions.get('window').width }}
+          swipeEnabled={true}
+          renderTabBar={props => (
+            <TabBar
+              {...props}
+              indicatorStyle={defaultTabsStyles.itemSelectedText}
+              style={defaultTabsStyles.tabsBar}
+              labelStyle={defaultTabsStyles.itemText}
+              scrollEnabled
+              tabStyle={{ width: 'auto' }}
+              renderLabel={({ route, focused }) => (
+                <Text
+                  style={
+                    focused
+                      ? defaultTabsStyles.selectedLabel
+                      : defaultTabsStyles.itemText
+                  }>
+                  {route.title}
+                </Text>
+              )}
+            />
+          )}
         />
-        <TabComponent />
       </View>
     );
   }
@@ -66,5 +75,31 @@ export const defaultTabsStyles = StyleSheet.create({
     marginRight: 13,
     paddingTop: 5,
     paddingBottom: 5
+  },
+  tabsBar: {
+    backgroundColor: 'transparent'
+  },
+  itemSelectedText: {
+    display: 'none',
+    height: 0
+  },
+  selectedLabel: {
+    paddingTop: 4,
+    color: colorBlack,
+    fontSize: 16,
+    fontFamily: boldFont,
+    letterSpacing: 2,
+    alignSelf: 'center',
+    textAlign: 'left',
+    overflow: 'visible'
+  },
+  itemText: {
+    color: colorVioletGrey,
+    fontSize: 16,
+    fontFamily: semiboldFont,
+    alignSelf: 'center',
+    textAlign: 'left',
+    letterSpacing: 2,
+    paddingTop: 4
   }
 });
